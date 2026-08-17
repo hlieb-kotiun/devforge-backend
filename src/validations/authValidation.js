ddbd982250bcc48fafc536ba6ae6a74145636088
VolodymyrB <volodymyrbedryk@gmail.com>
2026-08-17 12:13:24 +0200

    added trim

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,15 +2,15 @@ import { Joi, Segments } from 'celebrate';
 
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
-    username: Joi.string().min(2).max(32).required(),
-    email: Joi.string().email().lowercase().max(64).required(),
+    username: Joi.string().trim().min(2).max(32).required(),
+    email: Joi.string().trim().email().lowercase().max(64).required(),
     password: Joi.string().min(8).max(64).required(),
   }),
 };
 
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
-    email: Joi.string().email().lowercase().max(64).required(),
+    email: Joi.string().trim().email().lowercase().max(64).required(),
     password: Joi.string().min(8).max(64).required(),
   }),
 };
